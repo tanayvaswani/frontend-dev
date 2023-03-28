1c3a41b66e2b79b5f211e751ea95689031ec2799
Tanay Vaswani <vaswani.tanay9@gmail.com>
2023-03-29 02:07:48 +0530

    started JS

---
+++ JavaScript/JS 1,2/index.js
@@ -0,0 +1,8 @@
+// this is a comment 
+// datatypes in JS 
+//
+
+console.log("Hello, World");
+console.log(typeof "Tanay");
+console.log(typeof 42);
+console.log(typeof true);
---
+++ JavaScript/JS3/index.js
@@ -0,0 +1,39 @@
+// String
+const myVariable = "Mathematics";
+
+// Length of string
+console.log(myVariable.length);
+console.log("Every good boy does fine".length);
+
+// charAt method
+console.log(myVariable.charAt(4));
+
+// indexOf method
+console.log(myVariable.lastIndexOf(""));
+
+// slice method
+console.log(myVariable.slice(5, 8));
+console.log(myVariable.slice(5));
+console.log(myVariable.slice(5, 6));
+console.log(myVariable.slice(5, 5));
+
+// toUpperCase method
+console.log(myVariable.toUpperCase());
+
+// toLowerCase method
+console.log(myVariable.toLowerCase());
+
+// includes method // true or false
+console.log(myVariable.includes("cd"));
+
+// split method // ouput in array
+console.log(myVariable.split("a"));
+console.log(myVariable.split(""));
+console.log("Tanay, Vaswani, Loves, DevOps".split(","));
+console.log("Tanay Vaswani is placed at $450K in San-Francisco right after college!".split(" "));
+
+
+
+
+
+
